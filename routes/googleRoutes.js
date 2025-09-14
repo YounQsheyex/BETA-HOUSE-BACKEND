683d715ddbc502030a7377bc1d064e14967d801c
YounQsheyex <younqsheyex@gmail.com>
2025-09-14 14:59:00 -0700

    update

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -1,16 +1,22 @@
 const router = require("express").Router();
-const passport=require("../controllers/googleAuth")
+const passport = require("../controllers/googleAuth");
 
 router.get(
   "/google/signup",
-  passport.authenticate("google-signup", { scope: ["profile", "email"] })
+  passport.authenticate("google-signup", {
+    scope: ["profile", "email"],
+    session: false,
+  })
 );
 
 router.get(
   "/google/signup/callback",
-  passport.authenticate("google-signup", { failureRedirect: "/signup-failed" }),
+  passport.authenticate("google-signup", {
+    failureRedirect: "/signup-failed",
+    session: false,
+  }),
   (req, res) => {
     res.redirect(`${process.env.FRONTEND_URL}/sign-in`);
   }
 );
-module.exports=router
+module.exports = router;
