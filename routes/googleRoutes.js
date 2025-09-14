0084ed45deca33fa18428874ff63d30857115aff
YounQsheyex <younqsheyex@gmail.com>
2025-09-14 08:16:21 -0700

    feat:added google Oauth20

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -2,12 +2,12 @@ const router = require("express").Router();
 const passport=require("../controllers/googleAuth")
 
 router.get(
-  "/auth/google/signup",
+  "/google/signup",
   passport.authenticate("google-signup", { scope: ["profile", "email"] })
 );
 
 router.get(
-  "/auth/google/signup/callback",
+  "/google/signup/callback",
   passport.authenticate("google-signup", { failureRedirect: "/signup-failed" }),
   (req, res) => {
     res.redirect(`${process.env.FRONTEND_URL}/sign-in`);
